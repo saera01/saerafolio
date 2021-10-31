7a4a28b2e91c9f1405de065df26f9c0f631311b1
Taeyoon Lee <taeyoon0137@gmail.com>
2021-10-31 19:02:38 +0900

    add: typescript setting

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import Component from './Component';
+import App from './App';
 
-render(<Component />, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
